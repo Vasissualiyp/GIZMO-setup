867a4c86f3ac551f2d46379c51ef89bd32153c4f
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-03-31 12:02:08 -0400

    Added code that would generate the snapshot times

---
+++ snapshottimes_generator.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# define the start time, end time, and number of snapshots
+t1 = 0.00990099
+t2 = 10
+n = 140
+
+# generate the geometric sequence of snapshot times
+times = np.logspace(np.log10(t1), np.log10(t2), n)
+#times = np.geomspace(t1, t2, n)
+
+# print the snapshot times
+print(times)
+
+# write the snapshot times to a file
+filename = "snapshot_times.txt"
+with open(filename, 'w') as f:
+	for t in times:
+		f.write(str(t) + "\n")
