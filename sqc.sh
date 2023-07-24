634354fa5528e6eb7ae556554958cf813b8b6393
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-07-24 12:07:16 -0400

    Added the job tracker that will work with Sunnyvale cluster

--- sqc.sh
+++ sqc.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+systemname=$(hostname)
+sleeptime=1
 # Move the cursor 1 line down
 echo -e "\033[1B"
 
-while true; do
-	  echo -ne "\033[2A" # Move the cursor to the beginning of the line
-	    sqc -u $USER
-	      sleep 1 # Adjust the sleep duration (in seconds) based on how often you want to update the list
-      done
-
+if [[ "$systemname" == "nia-login"*".scinet.local" ]]; then # Niagara
+	while true; do
+		  echo -ne "\033[2A" # Move the cursor to the beginning of the line
+		    sqc -u $USER
+		      sleep $sleeptime # Adjust the sleep duration (in seconds) based on how often you want to update the list
+	      done
+else # Sunnyvale
+	 while true; do 
+	        echo -ne "\033[2A" # Move the cursor to the beginning of the line
+		qstat -u $USER 
+		sleep $sleeptime 
+	done	
+fi
