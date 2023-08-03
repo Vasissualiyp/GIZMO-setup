23371f1a3e9a1343dfb5420bc88d758b55f03041
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-08-03 14:59:28 -0400

    The script now works with more than 1 job

--- scripts/sqc.sh
+++ scripts/sqc.sh
@@ -2,19 +2,53 @@
 
 systemname=$(hostname)
 sleeptime=1
+
 # Move the cursor 1 line down
 echo -e "\033[1B"
 
 if [[ "$systemname" == "nia-login"*".scinet.local" ]]; then # Niagara
 	while true; do
-		  echo -ne "\033[2A" # Move the cursor to the beginning of the line
-		    sqc -u $USER
-		      sleep $sleeptime # Adjust the sleep duration (in seconds) based on how often you want to update the list
-	      done
+                # Collect command output to a variable
+                output=$(sqc -u $USER)
+                # Calculate number of lines in the output
+                lines=$(echo "$output" | wc -l)
+                # Adjust the number of lines
+                lines=$((lines-2))
+
+                # Move the cursor to the beginning of the line
+                echo -ne "\033[2A"
+
+                # Print command output
+                echo "$output"
+
+                # Move the cursor up by number of lines in the output
+                if [ $lines -gt 0 ]; then
+                    tput cuu $lines
+                fi
+
+                sleep $sleeptime # Adjust the sleep duration (in seconds) based on how often you want to update the list
+	done
 else # Sunnyvale
-	 while true; do 
-	        echo -ne "\033[2A" # Move the cursor to the beginning of the line
-		qstat -u $USER 
-		sleep $sleeptime 
+	while true; do
+                # Collect command output to a variable
+                output=$(qstat -u $USER)
+                # Calculate number of lines in the output
+                lines=$(echo "$output" | wc -l)
+                # Adjust the number of lines
+                lines=$((lines-2))
+
+                # Move the cursor to the beginning of the line
+                echo -ne "\033[2A"
+
+                # Print command output
+                echo "$output"
+
+                # Move the cursor up by number of lines in the output
+                if [ $lines -gt 0 ]; then
+                    tput cuu $lines
+                fi
+
+                sleep $sleeptime 
 	done	
 fi
+
