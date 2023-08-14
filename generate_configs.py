09d217b9e372c4c206719d73285387466668c44a
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-08-14 13:47:22 -0400

    Added all necessary generators

--- generate_configs.py
+++ generate_configs.py
@@ -5,10 +5,16 @@ parameters = {
     "Config.sh": [
         ("PMGRID", [16, 32]),
         ("MULTIPLEDOMAINS", [16, 32]),
+        ("OPENMP", [16, 32]),
     ],
     "zel.params": [
         ("TimeBetSnapshots", [0.01, 0.02]),
     ],
+}
+
+# Define parameters to remove only for the first run
+remove_first_run = {
+    #"Config.sh": ["OPENMP"], # Example of removal only for the first run
 }
 
 # Create an empty list to store the lines
@@ -18,7 +24,16 @@ lines = []
 for filename, params in parameters.items():
     # Extract the parameter names and values
     param_names, param_values = zip(*params)
-    
+
+    # Handle removal only for the first run
+    if filename in remove_first_run:
+        param_str = " ".join(f"{name}={value}" for name, value in zip(param_names, param_values[0]))
+        for param_to_remove in remove_first_run[filename]:
+            param_str += f" -{param_to_remove}"
+        line = f"{filename}: {param_str}"
+        lines.append(line)
+        lines.append("###")
+
     # Iterate through the cartesian product of the parameter values
     for values in product(*param_values):
         # Combine the parameter names and values
