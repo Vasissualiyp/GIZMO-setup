b2c7f3db875720d360191b69f2cf5a5906956544
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-08-14 12:11:13 -0400

    It is now possible to customize which parameters exactly we need to generate the config files for

--- generate_configs.py
+++ generate_configs.py
@@ -1,19 +1,32 @@
 from itertools import product
 
-# Define the values for the parameters
-pmgrid_values = [16, 32]
-multipledomains_values = [16, 32]
-timebetsnapshots_values = [0.01, 0.02]
+# Define the parameters and their values
+parameters = {
+    "Config.sh": [
+        ("PMGRID", [16, 32]),
+        ("MULTIPLEDOMAINS", [16, 32]),
+    ],
+    "zel.params": [
+        ("TimeBetSnapshots", [0.01, 0.02]),
+    ],
+}
 
 # Create an empty list to store the lines
 lines = []
 
-# Iterate through the cartesian product of the parameter values
-for pmgrid, multipledomains, timebetsnapshots in product(pmgrid_values, multipledomains_values, timebetsnapshots_values):
-    config_line = f"Config.sh: PMGRID={pmgrid} MULTIPLEDOMAINS={multipledomains}"
-    params_line = f"zel.params: TimeBetSnapshots={timebetsnapshots}"
-    lines.append(config_line)
-    lines.append(params_line)
+# Iterate through the files and their parameters
+for filename, params in parameters.items():
+    # Extract the parameter names and values
+    param_names, param_values = zip(*params)
+    
+    # Iterate through the cartesian product of the parameter values
+    for values in product(*param_values):
+        # Combine the parameter names and values
+        param_str = " ".join(f"{name}={value}" for name, value in zip(param_names, values))
+        line = f"{filename}: {param_str}"
+        lines.append(line)
+
+    # Add separator
     lines.append("###")
 
 # Write the lines to the configs.txt file
