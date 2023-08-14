e898c2e48f8f14e3e6d79ba684fb8955455b7b96
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-08-14 16:43:56 -0400

    The editing of zel.params file's softenings doesn't work. -PARAMETER also doesn't work for the generator of parameter grids

--- generate_configs.py
+++ generate_configs.py
@@ -1,19 +1,25 @@
+import numpy as np
 from itertools import product
 
 # Define the parameters and their values
+generated_softenings = np.ones(5) * 1000 / 128
+for i in range(np.size(generated_softenings)):
+    generated_softenings[i] = generated_softenings[i] * 10**(-i)
+
 parameters = {
     "Config.sh": [
-        ("PMGRID", [16, 32]),
-        ("MULTIPLEDOMAINS", [16, 32]),
+        #("PMGRID", [16, 32]),
+        #("MULTIPLEDOMAINS", [16, 32]),
     ],
     "zel.params": [
-        ("TimeBetSnapshot", [0.01, 0.02]),
+        ("Softening_Type0", generated_softenings.tolist()), # Convert to Python list
     ],
 }
 
+
 # Define parameters to remove only for the first run
 remove_first_run = {
-    "Config.sh": ["OPENMP"], # Example of removal only for the first run
+    "Config.sh": ["PMGRID", "MULTIPLEDOMAINS"], # Example of removal only for the first run
 }
 
 # Create an empty list to store the lines
@@ -21,24 +27,25 @@ lines = []
 
 # Iterate through the files and their parameters
 for filename, params in parameters.items():
-    # Extract the parameter names and values
-    param_names, param_values = zip(*params)
+    if params: # Check if params not empty
+        # Extract the parameter names and values
+        param_names, param_values = zip(*params)
 
-    # Handle removal only for the first run
-    if filename in remove_first_run:
-        param_str = " ".join(f"{name}={value}" for name, value in zip(param_names, param_values[0]))
-        for param_to_remove in remove_first_run[filename]:
-            param_str += f" -{param_to_remove}"
-        line = f"{filename}: {param_str}"
-        lines.append(line)
-        lines.append("###")
+        # Handle removal only for the first run
+        if filename in remove_first_run:
+            param_str = " ".join(f"{name}={value}" for name, value in zip(param_names, param_values[0]))
+            for param_to_remove in remove_first_run[filename]:
+                param_str += f" -{param_to_remove}"
+            line = f"{filename}: {param_str}"
+            lines.append(line)
+            lines.append("###")
 
-    # Iterate through the cartesian product of the parameter values
-    for values in product(*param_values):
-        # Combine the parameter names and values
-        param_str = " ".join(f"{name}={value}" for name, value in zip(param_names, values))
-        line = f"{filename}: {param_str}"
-        lines.append(line)
+        # Iterate through the cartesian product of the parameter values
+        for values in product(*param_values):
+            # Combine the parameter names and values
+            param_str = " ".join(f"{name}={value}" for name, value in zip(param_names, values))
+            line = f"{filename}: {param_str}"
+            lines.append(line)
 
     # Add separator
     lines.append("###")
