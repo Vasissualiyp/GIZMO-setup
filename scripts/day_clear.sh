b4a6c0a216e3212f5a372423e819a367722de944
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-08-15 09:59:06 -0400

    Added a script that can clean the entire day's work and start over

---
+++ scripts/day_clear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get today's date in YYYY-MM-DD format
+TODAY=$(date +"%Y-%m-%d")
+
+# Remove directories matching the pattern
+rm -f -r ./archive/${TODAY}*
+
+# Subtract a day from today's date
+YESTERDAY=$(date -d "${TODAY} -1 day" +"%Y.%m.%d")
+
+# Use vim to find and replace the date in the file
+vim -c "%s/OutputDir\s*\.\/output\/[0-9]\{4\}\.[0-9]\{2\}\.[0-9]\{2\}:/OutputDir                               .\/output\/${YESTERDAY}:/g" -c 'wq' zel.params
+
