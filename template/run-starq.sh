b1b3d43b46043e91ff91710d2ffa2d15aaf792cc
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-10-24 15:03:00 -0400

    Updated the makefile for starq

--- template/run-starq.sh
+++ template/run-starq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
-#PBS -l nodes=1:ppn=128
-#PBS -l walltime=1:00:00
+#PBS -l nodes=3:ppn=128
+#PBS -l walltime=12:00:00
 #PBS -r n
 #PBS -j oe
 #PBS -q starq
@@ -18,7 +18,7 @@
 
 module purge
 #module load intel intelmpi gsl/2.7.1-intel-19.1.3 hdf5/1.12.1-intel
-module load gcc/11.2.0 openmpi/4.1.2-ucx gsl/2.7.1 hdf5/1.12.1-ucx fftw/3.3.10-openmpi-ucx 
+module load gcc/13.2.0 openmpi/4.1.6-gcc-ucx gsl/2.7.1 hdf5/1.12.1-ucx fftw/3.3.10-openmpi-ucx
 
 
 # go to your working directory containing the batch script, code and data
@@ -28,12 +28,14 @@ cd $PBS_O_WORKDIR
 #echo $PATH
 
 cd gizmo
+rm ./GIZMO
 make clean
-make -j 10
+make -j 20
 cd ..
 
-export OMP_NUM_THREADS=128
+export OMP_NUM_THREADS=2
 
 #mpirun -np 128 ./GIZMO gizmo_parameters.txt > dummy.out
-mpirun -np 128 ./gizmo/GIZMO zel.params 
+mpirun -np 192 -map-by node:SPAN ./gizmo/GIZMO zel.params #>& output.log
+#mpirun -np 128 ./gizmo/GIZMO zel.params 
 
