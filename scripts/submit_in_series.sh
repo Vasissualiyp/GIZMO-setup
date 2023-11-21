c96976a52af25d4a54f24b0f921275a776640bad
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-11-21 12:53:50 -0500

    Working performance tester for both niagara and sunnyvale

--- scripts/submit_in_series.sh
+++ scripts/submit_in_series.sh
@@ -3,9 +3,35 @@
 systemname=$(hostname)
 sleeptime=5 # Sleep time in sec
 
+change_number_of_processes_starq() { #{{{
+    local file_path="./template/run-starq.sh" # Path to your SLURM script
+    
+    # Extract ppn value
+    ppn=$(awk -F 'nodes=|:ppn=' '/#PBS -l nodes=/ {print $3}' "$file_path")
+    #echo "$ppn"
+    
+    # Extract OMP_NUM_THREADS value
+    openmp_threads=$(grep -Po '(?<=export OMP_NUM_THREADS=)\d+' "$file_path")
+    #echo "$openmp_threads"
+    
+    # Your new node count
+    local new_nodes="$1" # The new number of nodes
+    
+    # Calculate new number of processes
+    new_processes=$((new_nodes * ppn / openmp_threads))
+    #echo "$new_processes"
+    
+    # Replace the number of processes in the mpirun command
+    sed -i "/^mpirun -np [0-9]* -map-by node:SPAN/c\mpirun -np $new_processes -map-by node:SPAN ./gizmo/GIZMO zel.params > output.log" "$file_path"
+} #}}}
+
 update_nodes() { #{{{ Update the number of nodes. Works on Niagara.
     local new_nodes="$1" # The new number of nodes
-    local file_path="./template/run.sh" # Path to your SLURM script
+    if [[ "$systemname" == "nia-login"*".scinet.local" ]]; then # Niagara
+        local file_path="./template/run.sh" # Path to your SLURM script
+    else # Sunnyvale
+        local file_path="./template/run-starq.sh" # Path to your SLURM script
+    fi
 
     # Check if the file exists
     if [[ ! -f "$file_path" ]]; then
@@ -14,7 +40,12 @@ update_nodes() { #{{{ Update the number of nodes. Works on Niagara.
     fi
 
     # Update the number of nodes using sed
-    sed -i "s/#SBATCH --nodes=[0-9]*/#SBATCH --nodes=${new_nodes}/" "$file_path"
+    if [[ "$systemname" == "nia-login"*".scinet.local" ]]; then # Niagara
+        sed -i "s/#SBATCH --nodes=[0-9]*/#SBATCH --nodes=${new_nodes}/" "$file_path"
+    else # Sunnyvale
+	sed -i "s/#PBS -l nodes=[0-9]*:ppn=128/#PBS -l nodes=${new_nodes}:ppn=128/" "$file_path"
+	change_number_of_processes_starq "$new_nodes"
+    fi
 } #}}}
 
 get_nodes() { #{{{
@@ -28,7 +59,7 @@ get_nodes() { #{{{
 
     # Read the first line (the first integer) and store it in a variable
     read -r nodes_number < "$file_path"
-    echo "$nodes_number"
+    #echo "$nodes_number"
 
     # Remove the first line from the file
     sed -i '1d' "$file_path"
@@ -53,13 +84,22 @@ while true; do
 	        output=$(qstat -u $USER)
 	        # Calculate number of lines in the output
 	        lines=$(echo "$output" | wc -l)
+	        lines=$((lines-1))
 	
 	fi
+	#echo "Lines are: $lines"
 
 	if [[ $lines -lt 1 ]]; then
 	    nodes_number=$(get_nodes)   # Get the number of nodes from the file
-	    update_nodes "$nodes_number"  # Update the number of nodes in run.sh
+	    #echo "Nodes number: $nodes_number"
+	    sleep 1
+	    if [ ${#nodes_number} -gt 10 ]; then
+		exit 0
+	    else
+	        update_nodes "$nodes_number"  # Update the number of nodes in run.sh
+	    fi
 
+	    #echo "Done"
 	    ./compile_autosub.sh
 	fi
 
