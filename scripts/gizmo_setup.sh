10c08fd32be91822c7a8b56f1a8f60c27255f7c8
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-07-01 16:54:23 -0400

    Added pp-music interfact to possible installable programs

--- scripts/gizmo_setup.sh
+++ scripts/gizmo_setup.sh
@@ -24,6 +24,7 @@ pull_git() {
   echo ""
   echo "a) GIZMO analysis"
   echo "m) MUSIC (Cosmological ICs generation)"
+  echo "i) PeakPatch-MUSIC Interface"
   echo "r) Rockstar (Halo Finder)"
   echo "g) GRACKLE (Thermochemistry library)"
   echo ""
@@ -35,27 +36,27 @@ pull_git() {
   # Clone the selected repo
   case $choice in
     1)
-		mkdir gizmo
+    mkdir gizmo
     git clone git@bitbucket.org:phopkins/gizmo-public.git gizmo || echo "Failed to copy GIZMO - make sure to add your machine's ssh key on BitBucket"
     program_to_setup="gizmo"
     ;;
   2)
-	  mkdir gizmo
+    mkdir gizmo
     git clone git@bitbucket.org:phopkins/gizmo.git || echo "Failed to copy GIZMO - make sure to add your machine's ssh key on BitBucket"
     program_to_setup="gizmo"
     ;;
   3)
-	  mkdir gizmo
+    mkdir gizmo
     git clone git@bitbucket.org:guszejnov/gizmo_imf.git gizmo  || echo "Failed to copy GIZMO - make sure to add your machine's ssh key on BitBucket"
     program_to_setup="gizmo"
     ;;
   a)
-	  mkdir analysis
+    mkdir analysis
     git clone https://github.com/Vasissualiyp/GIZMO-analysis.git analysis  || echo "Failed to git clone the analysis directory"
     program_to_setup="gizmo"
     ;;
   p)
-	  mkdir peakpatch
+    mkdir peakpatch
     git clone git@gitlab.com:natecarlson/peakpatch.git peakpatch  || echo "Failed to git clone the Peak Patch directory"
     program_to_setup="peakpatch"
     ;;
@@ -78,6 +79,22 @@ pull_git() {
     rm -rf musictmp
     cd music
     program_to_setup="music"
+    ;;
+  i)
+    # FIRST, CLONE PeakPatch:
+    mkdir peakpatch
+    git clone git@gitlab.com:natecarlson/peakpatch.git peakpatch  || echo "Failed to git clone the Peak Patch directory"
+    # SECOND, CLONE MUSIC:
+    # Create temporary directory to store git-tracked parts of music
+    mkdir musictmp
+    mv -r music/* musictmp/
+    git clone git@bitbucket.org:vpustovoit/music.git || echo "Failed to git clone music"
+    mv -r musictmp/* music/
+    rm -rf musictmp
+    program_to_setup="music+pp"
+	# FINALLY, CLONE pp-music-interface:
+	git clone https://github.com/Vasissualiyp/pp-music-interface pp-music-interface \
+			|| echo "Failed to clone pp-music-interface"
     ;;
   r)
     # Create temporary directory to store git-tracked parts of rockstar
@@ -112,48 +129,48 @@ if [ "$program_to_setup" == "gizmo" ]; then
     sed -i '/^[^#]/ s/^/#/' Makefile.systype
     #sed -i '29s/^.//' Makefile.systype
     if [[ "$systemname" == "nia-login"*".scinet.local" ]]; then # Niagara
-    	echo "\nSYSTYPE=\"SciNet\"" >> Makefile.systype
+        echo "\nSYSTYPE=\"SciNet\"" >> Makefile.systype
     else # Sunnyvale
-    	echo "\nSYSTYPE=\"CITA-starq\"" >> Makefile.systype
+        echo "\nSYSTYPE=\"CITA-starq\"" >> Makefile.systype
     
-    	# Path to the Makefile
-    	makefile_path="./Makefile"
-    	
-    	# Path to the CITA-starqMakefile.txt
-    	cita_file_path="../template/CITA-starqMakefile.txt"
-    	
-    	# Temporary file for the new Makefile
-    	temp_file_path="../template/temp.txt"
-    	
-    	# Use awk to insert the contents of CITA-starqMakefile.txt into Makefile
-    	awk -v r="$cita_file_path" '
-    	    {
-    	        lines[NR] = $0;
-    	    }
-    	    NR==1 {
-    	        next;
-    	    }
-    	    NR==2 {
-    	        next;
-    	    }
-    	    NR==3 {
-    	        next;
-    	    }
-    	    NR>3 && lines[NR-3] == "ifeq ($(SYSTYPE),\"CITA\")" {
-    	        while ((getline line < r) > 0) {
-    	            print line
-    	        }
-    	    }
-    	    {
-    	        print lines[NR-3]
-    	    }
-    	    END {
-    	        print lines[NR-1];
-    	        print lines[NR];
-    	    }' "$makefile_path" > "$temp_file_path"
-    	
-    	# Replace the original Makefile with the new Makefile
-    	mv "$temp_file_path" "$makefile_path"
+        # Path to the Makefile
+        makefile_path="./Makefile"
+        
+        # Path to the CITA-starqMakefile.txt
+        cita_file_path="../template/CITA-starqMakefile.txt"
+        
+        # Temporary file for the new Makefile
+        temp_file_path="../template/temp.txt"
+        
+        # Use awk to insert the contents of CITA-starqMakefile.txt into Makefile
+        awk -v r="$cita_file_path" '
+            {
+                lines[NR] = $0;
+            }
+            NR==1 {
+                next;
+            }
+            NR==2 {
+                next;
+            }
+            NR==3 {
+                next;
+            }
+            NR>3 && lines[NR-3] == "ifeq ($(SYSTYPE),\"CITA\")" {
+                while ((getline line < r) > 0) {
+                    print line
+                }
+            }
+            {
+                print lines[NR-3]
+            }
+            END {
+                print lines[NR-1];
+                print lines[NR];
+            }' "$makefile_path" > "$temp_file_path"
+        
+        # Replace the original Makefile with the new Makefile
+        mv "$temp_file_path" "$makefile_path"
     fi
     
     cp ./cooling/TREECOOL ../TREECOOL
@@ -180,23 +197,40 @@ if [ "$program_to_setup" == "gizmo" ]; then
 # }}}
 elif [ "$program_to_setup" == "music" ]; then 
 # Make MUSIC {{{
-    module load gcc hdf5 gsl fftw
+    module load gcc hdf5 gsl fftw || echo "modules are unavailable for current system"
     make -j20
-		cp ../template/*.conf ./
+    cp ../template/*.conf ./
+#}}}
+elif [ "$program_to_setup" == "music+pp" ]; then 
+# Make MUSIC {{{
+    cd music
+    module load gcc hdf5 gsl fftw python cfitsio || echo "modules are unavailable for current system"
+    make -j20
+    cp ../template/*.conf ./
+	cd .. # Return to main dir
+# Make PeakPatch
+    source ~/.bashrc
+    cd peakpatch || exit 1
+    python -m venv env
+    source ./env/bin/activate
+    pip install matplotlib numpy healpy
+    echo "Python environment for Peak Patch has been created"
+    echo "The script to make Peak Patch hasn't been set up yet"
+	cd .. # Return to main dir
 #}}}
 elif [ "$program_to_setup" == "rockstar" ]; then 
 # Make Rockstar {{{
-    module load hdf5
+    module load hdf5 || echo "modules are unavailable for current system"
     sed -i '2iCFLAGS += -I/usr/include/tirpc' Makefile
     sed -i '5iOFLAGS += -ltirpc' Makefile
     make with_hdf5
-		cp ../template/rockstar.cfg ./rockstar.cfg
+    cp ../template/rockstar.cfg ./rockstar.cfg
 #}}}
 elif [ "$program_to_setup" == "grackle" ]; then 
 # Make GRACKLE {{{
     ./configure
     cd src/clib
-    module load gcc hdf5
+    module load gcc hdf5 || echo "modules are unavailable for current system"
     make clean
     make machine-cita
     make -j20
@@ -204,11 +238,11 @@ elif [ "$program_to_setup" == "grackle" ]; then
 elif [ "$program_to_setup" == "peakpatch" ]; then 
 # Make PeakPatch {{{
     source ~/.bashrc
-    module load python
+    module load python || echo "modules are unavailable for current system"
     cd peakpatch || exit 1
     python -m venv env
     source ./env/bin/activate
-    pip install matplotlib numpy 
+    pip install matplotlib numpy healpy
     echo "Python environment for Peak Patch has been created"
     echo "The script to make Peak Patch hasn't been set up yet"
 # }}}
